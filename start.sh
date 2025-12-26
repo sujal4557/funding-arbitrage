86e804dadf7fb7dc8e9a7fa970de18917c8eb6ef
Sujal Agrawal <115213321+sujal4557@users.noreply.github.com>
2025-12-26 17:56:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+echo "Starting FastAPI..."
 uvicorn api:app --host 0.0.0.0 --port 8000 &
+
+echo "Starting Streamlit..."
 streamlit run app.py --server.port 8501 --server.address 0.0.0.0
